f8e4018110fac73c2464135b48bb05f668d14ce5
ZiyadHQ <ziyadhaqbani@gmail.com>
2025-01-01 01:42:28 +0300

    added variable resolution

--- Interpreter.java
+++ Interpreter.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     final Environment globals = new Environment();
     private Environment environment = globals;
+    private final Map<Expr, Integer> locals = new HashMap<Expr,Integer>();
 
     Interpreter(){
         NativeFunctions.registerFunctions(globals);
@@ -120,8 +123,16 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     @Override
     public Object visitVariableExpr(Expr.Variable expr) {
-        Object value = environment.get(expr.name);
-        return value;
+        return lookUpVariable(expr.name, expr);
+    }
+
+    private Object lookUpVariable(Token name, Expr expr){
+        Integer distance = locals.get(expr);
+        if(distance != null){
+            return environment.getAt(distance, name.lexeme);
+        }else{
+            return globals.get(name);
+        }
     }
 
     private Boolean isTruthy(Object object) {
@@ -162,6 +173,10 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     private void execute(Stmt stmt){
         stmt.accept(this);
+    }
+
+    void resolve(Expr expr, int depth){
+        locals.put(expr, depth);
     }
 
     void executeBlock(List<Stmt> statements, Environment environment){
@@ -205,6 +220,14 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     public Object visitAssignExpr(Expr.Assign expr) {
         Object value = evaluate(expr.value);
         environment.assign(expr.name, value);
+
+        Integer distance = locals.get(expr);
+        if(distance != null){
+            environment.assignAt(distance, expr.name, value);
+        }else{
+            environment.assign(expr.name, value);
+        }
+
         return value;
     }
 
